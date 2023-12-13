6802614e522d1bf2025cd6cb4736a6d938462e28
ValentinoJerez <valenjerez69@gmail.com>
2023-12-13 12:45:56 -0300

    Home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,13 +1,10 @@
+import style from "../Views/Home.module.css"
 import NavBar from "../Components/NavBar/NavBar"
 
 function Home() {
     return (
-        <div>
-            <style>{`body {margin: 0;}`}</style>
-            <div className="bg-cover h-screen" style={{ backgroundImage: "url('src/assets/Fondo-constellation.png')" }}>
-                <NavBar />
-                <h1 className="text-white text-4xl font-bold">Hola</h1>
-            </div>
+        <div className={style.Home}>
+            <NavBar />
         </div>
     )
 }
