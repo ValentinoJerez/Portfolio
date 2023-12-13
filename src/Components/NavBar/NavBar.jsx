3ec376cf7995f3df8e58ece27cf75fac0752f068
ValentinoJerez <valenjerez69@gmail.com>
2023-12-13 14:37:50 -0300

    NavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,26 +1,20 @@
 import { Link } from "react-router-dom";
+import style from "../NavBar/NavBar.module.css"
+
+import Icono from "../../assets/Icono-Cohete.png"
 
 function NavBar() {
     return (
-        <nav class="navbar navbar-expand-md navbar-light">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                </button>
-                <div class="collapse navbar-collapse" id="navbar-toggler">
-                    <img src="Imagenes/Icono-Cohete.png" width="50" alt="Logo Pagina Web" />
-                    <ul class="navbar-nav" d-flex justify-content-center align-items-center>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Sobre mi</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Educacion</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contacto</a>
-                        </li>
+        <nav>
+            <div class="navbar-container">
+                <button type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation"></button>
+                <div id="navbar-toggler">
+                    <img src={Icono} width="50" alt="Logo Pagina Web" />
+                    <ul class="navbar-menu">
+                        <li><a aria-current="page" href="#">Sobre mí</a></li>
+                        <li><a href="#">Educación</a></li>
+                        <li><a href="#">Proyectos</a></li>
+                        <li><a href="#">Contacto</a></li>
                     </ul>
                 </div>
             </div>
