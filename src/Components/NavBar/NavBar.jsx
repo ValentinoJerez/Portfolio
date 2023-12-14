b264be5a8fa638678740406f243584b6c4e243a3
ValentinoJerez <valenjerez69@gmail.com>
2023-12-14 19:05:52 -0300

    ListoNavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,20 +5,20 @@ import Icono from "../../assets/Icono-Cohete.png"
 
 function NavBar() {
     return (
-        <nav>
-            <div class="navbar-container">
-                <button type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation"></button>
+        <nav className={style.container}>
+                <div className={style.Icono}>
+                    <img src={Icono} className={style.logo} alt="Logo Pagina Web" />
+                </div>
                 <div id="navbar-toggler">
-                    <img src={Icono} width="50" alt="Logo Pagina Web" />
-                    <ul class="navbar-menu">
+                    <ul className={style.menu}>
                         <li><a aria-current="page" href="#">Sobre mí</a></li>
                         <li><a href="#">Educación</a></li>
                         <li><a href="#">Proyectos</a></li>
                         <li><a href="#">Contacto</a></li>
                     </ul>
                 </div>
-            </div>
         </nav>
+
     )
 }
 
