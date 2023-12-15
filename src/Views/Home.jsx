004242912e2aff0e84745724612b76b9e71b6daf
ValentinoJerez <valenjerez69@gmail.com>
2023-12-15 09:39:53 -0300

    Cambios en Home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,9 +1,8 @@
-import style from "../Views/Home.module.css"
 import NavBar from "../Components/NavBar/NavBar"
 
 function Home() {
     return (
-        <div className={style.Home}>
+        <div>
             <NavBar />
         </div>
     )
