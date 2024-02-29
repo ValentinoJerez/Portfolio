198e341fdcbba596c8775f188b9d0bdb794ff652
ValentinoJerez <valenjerez69@gmail.com>
2024-02-29 17:09:37 -0300

    PresentacionListo

--- src/views/Inicio/Inicio.jsx
+++ src/views/Inicio/Inicio.jsx
@@ -8,19 +8,21 @@ function Inicio() {
         <div>
             <NavBar />
             <section className={style.Presentacion}>
-                <h1>Hola, mi nombre es Valentino Jerez y soy desarrollador web full stack</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia soluta veritatis facere non consequuntur provident
-                    quas possimus tempore nesciunt incidunt, exercitationem sequi atque maxime, beatae ex asperiores, id veniam a.</p>
-                <div className={style.Redes}>
-                    <ul className={style.ListaRedes}>
-                        <li><a href="https://www.linkedin.com/in/valentino-jerez/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
-                        <li><a href="https://github.com/ValentinoJerez" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                        <li><a href="https://www.behance.net/valentinojerez" target="_blank" rel="noopener noreferrer">Behance</a></li>
-                        <li><a href="https://www.instagram.com/valen_jerez03/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-                        <li><a href="http://" target="_blank" rel="noopener noreferrer">Curriculum</a></li>
-                    </ul>
+                <div className={style.Contenido}>
+                    <h1>Hola, mi nombre es Valentino Jerez y soy desarrollador web full stack</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia soluta veritatis facere non consequuntur provident
+                        quas possimus tempore nesciunt incidunt, exercitationem sequi atque maxime, beatae ex asperiores, id veniam a.</p>
+                    <div className={style.Redes}>
+                        <ul className={style.ListaRedes}>
+                            <li><a href="https://www.linkedin.com/in/valentino-jerez/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+                            <li><a href="https://github.com/ValentinoJerez" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                            <li><a href="https://www.behance.net/valentinojerez" target="_blank" rel="noopener noreferrer">Behance</a></li>
+                            <li><a href="https://www.instagram.com/valen_jerez03/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                            <li><a href="http://" target="_blank" rel="noopener noreferrer">Curriculum</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <img src={Perfil} alt="Foto Perfil" className={style.ImagenPerfil}/>
+                <img src={Perfil} alt="Foto Perfil" className={style.ImagenPerfil} />
             </section>
         </div>
     )
